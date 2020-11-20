aee147b1c22ce553fbbda2d3e1cdd2b754b6c46f
Michael Knight <msktenn@gmail.com>
2020-11-19 19:30:05 -0500

    Dockertest

---
+++ dockertest/server/Widgetsphere.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Widgetsphere.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
