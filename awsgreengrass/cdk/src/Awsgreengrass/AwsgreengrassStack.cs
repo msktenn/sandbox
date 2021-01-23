6a4c06d7f09d54459397b494df8074a3620f14ce
Michael Knight <msktenn@gmail.com>
2021-01-23 11:39:34 -0500

    add greengrass

---
+++ awsgreengrass/cdk/src/Awsgreengrass/AwsgreengrassStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+
+namespace Awsgreengrass
+{
+    public class AwsgreengrassStack : Stack
+    {
+        internal AwsgreengrassStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+
+        }
+    }
+}
---
+++ awsgreengrass/cdk/src/Awsgreengrass/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace Awsgreengrass
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AwsgreengrassStack(app, "AwsgreengrassStack");
+
+            app.Synth();
+        }
+    }
+}
