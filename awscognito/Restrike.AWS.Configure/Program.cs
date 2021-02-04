f3d566c2f0a5aea991bc327ddbe973e61493a331
Michael Knight <msktenn@gmail.com>
2021-02-03 22:39:25 -0500

    Playing with Amplify

---
+++ awscognito/Restrike.AWS.Configure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restrike.AWS.Configure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- awscognito/login/src/app/app.component.ts
+++ awscognito/login/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Auth } from 'aws-amplify'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'login';
+
+  async onClickMe() {
+    try {
+      //      const { user } = await Auth.configure();
+      Auth.
+        console.log("Hello World");
+    } catch (error) {
+      console.log('error signing up:', error);
+    }
+  }
+
 }
