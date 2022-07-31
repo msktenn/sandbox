470a1131f6e4c295d45512c4e3e9a8579b48acef
Michael Knight <michael.knight@onedatascan.com>
2022-07-31 17:45:23 -0400

    testing stuff

---
+++ micro-fes/api/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 8001;
+
+app.get("/", (req, res) => {
+  res.send("Hello from api");
+});
+
+app.listen(port, () => {
+  console.log(`api listening at http://localhost:${port}`);
+});
