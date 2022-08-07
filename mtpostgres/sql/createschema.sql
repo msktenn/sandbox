6b07752c447d389f7438578664316318dd4bf6c4
Michael Knight <michael.knight@onedatascan.com>
2022-08-06 20:54:47 -0400

    Move some stuff

--- mtpostgres/sql/createschema.sql
+++ mtpostgres/sql/createschema.sql
@@ -1,4 +1,10 @@
 /* create questions table with view to secure tenant */
+CREATE TABLE IF NOT EXISTS tenant (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name CITEXT NOT NULL
+);
+GRANT SELECT, INSERT, UPDATE, DELETE ON tenant TO tenant_admin;
+
 CREATE TABLE IF NOT EXISTS t_questions (
 	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	question_text CITEXT NOT NULL,
@@ -14,4 +20,4 @@ CREATE OR REPLACE VIEW questions AS
     WHERE
         tenant = current_user;
 
-GRANT SELECT, INSERT ON questions TO tenant_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON questions TO tenant_user;
--- mtpostgres/sql/dropdb.ps1
+++ mtpostgres/sql/dropdb.ps1
@@ -1,2 +1,3 @@
 $env:PGPASSWORD='33787bafa98847078fb363640ea87c6f'
 psql -U postgres -d postgres -a -f dropdatabase.sql
+
